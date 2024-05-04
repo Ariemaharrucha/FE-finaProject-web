f40b1103f443dfc2b41ef5af9d5b1d920d65bbc7
AndiMahardika <andi.mahardika.mulya@gmail.com>
2024-05-04 12:36:07 -0700

    create landing-page and template

---
+++ src/script/script.js
@@ -0,0 +1,26 @@
+window.addEventListener('scroll', function() {
+  const header = document.querySelector('.navbar');
+  
+  if(document.body.scrollTop > 20 || document.documentElement.scrollTop > 20){
+    header.classList.add('bg-navbar');
+    header.classList.remove('bg-transparent');
+    localStorage.setItem('scrollPosition', 'scrolled');
+  } else {
+    header.classList.remove('bg-navbar');
+    header.classList.add('bg-transparent');
+    localStorage.setItem('scrollPosition', 'top');
+  }
+});
+
+window.addEventListener('DOMContentLoaded', function() {
+  const header = document.querySelector('.navbar');
+  const scrollPosition = localStorage.getItem('scrollPosition');
+  
+  if (scrollPosition === 'scrolled') {
+    header.classList.add('bg-navbar');
+    header.classList.remove('bg-transparent');
+  } else {
+    header.classList.remove('bg-navbar');
+    header.classList.add('bg-transparent');
+  }
+});
